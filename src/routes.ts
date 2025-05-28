5aa36e312e16fc57f88d7279dd2daa684a334f91
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-05-28 12:46:41 +0800

    feat: Add forgot/reset password paths to auth routes definition
    Updated routes to include `/forgot-password` and `/reset-password` in the auth routes array. This ensures these paths are correctly handled by the authentication middleware.

--- src/routes.ts
+++ src/routes.ts
@@ -8,7 +8,7 @@ export const apiAuthPrefix: string = "/api/auth";
  * An array of routes used for authentication purposes
  * @type {string[]}
  */
-export const authRoutes: string[] = ["/signin", "/signup"];
+export const authRoutes: string[] = ["/signin", "/signup", "/forgot-password", "/reset-password"];
 
 /**
  * An array of routes that are accessible to the public, regardless of authentication status
