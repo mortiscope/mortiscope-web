6ba9f2c44d7e65f485d2a2e9c8ababacf039caa9
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-06-05 06:07:10 +0800

    refactor: Customize toaster component with theme colors and styles
    Updated the toaster component to use a custom, branded color palette. Defined CSS variables for default, success, error, and warning toast types to align with the application's theme. Also applied custom font and border classes using toast options.

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,11 +10,35 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast: "!font-inter !border-2",
+          title: "!font-normal",
+        },
+      }}
       style={
         {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
+          "--border-radius": "0.75rem",
+          
+          // Default/Info
+          "--normal-bg": "#e0f2fe",
+          "--normal-border": "#38bdf8",
+          "--normal-text": "#0369a1",
+
+          // Success
+          "--success-bg": "#d1fae5",
+          "--success-border": "#34d399",
+          "--success-text": "#065f46",
+
+          // Error
+          "--error-bg": "#ffe4e6",
+          "--error-border": "#fb7185",
+          "--error-text": "#9f1239",
+
+          // Warning
+          "--warning-bg": "#fef3c7",
+          "--warning-border": "#fbbf24",
+          "--warning-text": "#92400e",
         } as React.CSSProperties
       }
       {...props}
